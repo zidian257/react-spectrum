bd458c938f4e69b296319d09ba04e754ca50c1cf
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-07-14 15:29:03 +0200

    F

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 # PARCEL_WORKER_BACKEND=process yarn parcel --no-cache --no-hmr --no-source-maps \
 PARCEL_WORKER_BACKEND=process yarn parcel build --no-cache --no-source-maps --no-minify --no-scope-hoist \
   packages/@react-spectrum/checkbox/docs/Checkbox.mdx \
-  packages/@react-spectrum/form/docs/Form.mdx \
   packages/@react-spectrum/icon/docs/workflow-icons.mdx \
   packages/@react-spectrum/searchfield/docs/SearchField.mdx
