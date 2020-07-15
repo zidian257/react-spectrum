1029d9734a418d6c368da34c985834ed2ba1c198
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-07-15 16:32:12 +0200

    Add more info

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-# PARCEL_WORKER_BACKEND=process yarn parcel --no-cache --no-hmr --no-source-maps \
+# Execute run and browse to these pages:
+#   dist/react-spectrum/SearchField.html
+#   dist/react-spectrum/Checkbox.html
+#
+# SearchField is missing a CSS sibling (and therefore the text isn't centered)
+# See run.sh for the resulting graph (Searchfield.mdx's bundleGroup should contain the CSS bundle as well)
+#
+
 PARCEL_WORKER_BACKEND=process yarn parcel build --no-cache --no-source-maps --no-minify --no-scope-hoist \
   packages/@react-spectrum/checkbox/docs/Checkbox.mdx \
   packages/@react-spectrum/searchfield/docs/SearchField.mdx
